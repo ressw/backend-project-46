8480b60f2c0c904c38d04e95eaadd4e25abff84d
ressw <resswert@gmail.com>
2024-01-11 09:21:21 +0500

    Update src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,15 +1,23 @@
 import { program } from 'commander';
 
-const genDiff = () => {
-  console.log();
+const command = (argv) => {
+  for (let i = 0; i < argv.length; i += 1) {
+    console.log(`Hello, ${argv[i]}!`);
+  }
+};
 
+const genDiff = () => {
   program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .option('-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format');
+    .option('-f, --format <type>', 'output format')
+    .arguments('<files...>')
 
-  program.parse();
+    .action(command)
+    .parse(process.argv);
 };
 
 export default genDiff;
+
+genDiff();
