9a8f6029a8dff5fa00cc86aa7b7972bdcda8874d
ressw <resswert@gmail.com>
2024-05-22 20:44:46 +0500

    Update package.json, index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-undef */
 // import genDiff from '../index.js';
 
+const path1 = '../__fixtures__/file1.json';
+const path2 = '../__fixtures__/file2.json';
+
 test('test genDiff', () => {
+  console.log('path1:', path1);
+  console.log('path2:', path2);
+
   const res = '';
   console.log('wtf');
   expect(res).toBe('');
