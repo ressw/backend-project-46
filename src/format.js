cbdfa80b920400f76a38c77c4ba41b2d72265713
ressw <resswert@gmail.com>
2024-05-30 21:45:21 +0500

    Update src/format.js

--- src/format.js
+++ src/format.js
@@ -1,15 +1,28 @@
 /* eslint-disable no-unused-vars */
 import _ from 'lodash';
 
+const formatStr = (obj) => {
+  if (obj.type === 'deleted') {
+    return `- ${obj.key}: ${obj.value}`;
+  }
+
+  if (obj.type === 'added') {
+    return `+ ${obj.key}: ${obj.value}`;
+  }
+
+  return ` ${obj.key}: ${obj.value}`;
+};
+
 const format = (tree, formatStyle = '') => {
   console.log(tree);
   // console.log(formatStyle);
   const keys = Object.keys(tree);
+  const result = [];
+  let str = '';
   console.log(keys);
   if (!_.isObject(tree)) {
-    
+    str += formatStr(tree);
   }
-
 };
 
 export default format;
