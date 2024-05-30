c5b540772e53ac2e18f0cde91122bb3590065536
ressw <resswert@gmail.com>
2024-05-30 11:23:54 +0500

    Update src/format.js

--- src/format.js
+++ src/format.js
@@ -1,5 +1,15 @@
+/* eslint-disable no-unused-vars */
+import _ from 'lodash';
+
 const format = (tree, formatStyle = '') => {
-  console.log(formatStyle);
+  console.log(tree);
+  // console.log(formatStyle);
+  const keys = Object.keys(tree);
+  console.log(keys);
+  if (!_.isObject(tree)) {
+    
+  }
+
 };
 
 export default format;
