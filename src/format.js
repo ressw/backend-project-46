9af93c26d2086a532e4a0df4ef541963eeac9cd6
ressw <resswert@gmail.com>
2024-06-06 02:15:09 +0500

    Update src/format.js

--- src/format.js
+++ src/format.js
@@ -1,28 +1,43 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-unused-vars */
 import _ from 'lodash';
 
+const res = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
 const formatStr = (obj) => {
   if (obj.type === 'deleted') {
-    return `- ${obj.key}: ${obj.value}`;
+    return `  - ${obj.key}: ${obj.value}`;
   }
 
   if (obj.type === 'added') {
-    return `+ ${obj.key}: ${obj.value}`;
+    return `  + ${obj.key}: ${obj.value}`;
   }
 
-  return ` ${obj.key}: ${obj.value}`;
+  if (obj.type === 'changed') {
+    let str = `  - ${obj.key}: ${obj.value}\n`;
+    str += `  + ${obj.key}: ${obj.value2}`;
+    return str;
+  }
+
+  return `    ${obj.key}: ${obj.value}`;
 };
 
 const format = (tree, formatStyle = '') => {
-  console.log(tree);
-  // console.log(formatStyle);
-  const keys = Object.keys(tree);
   const result = [];
-  let str = '';
-  console.log(keys);
-  if (!_.isObject(tree)) {
-    str += formatStr(tree);
+  for (const item of tree) {
+    if (_.isObject(item)) {
+      result.push(formatStr(item));
+    }
   }
+  const str = `{\n${result.join('\n')}\n}`;
+  return str;
 };
 
 export default format;
