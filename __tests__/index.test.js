5328e6d00133f1c8143ef3466de5046b2b476dd0
ressw <resswert@gmail.com>
2024-06-07 04:28:56 +0500

    Update __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,31 +7,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path
   .join(__dirname, '..', '__fixtures__', filename);
+const res = getFixturePath('resultNestedJson.txt');
 
 test('genDiff test for flat JSON files', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
-  const res = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
   expect(genDiff(filepath1, filepath2)).toEqual(res);
 });
 
 test('genDiff test for flat YML files', () => {
   const filepath1 = getFixturePath('file1.yml');
   const filepath2 = getFixturePath('file2.yml');
-  const res = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
   expect(genDiff(filepath1, filepath2)).toEqual(res);
 });
