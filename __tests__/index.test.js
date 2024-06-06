9e8343515909064d01d16e5165330a9c342bbb9e
ressw <resswert@gmail.com>
2024-06-07 02:54:18 +0500

    Make __fixtures__/file1.yml,  __fixtures__/file2.yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,7 +8,21 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path
   .join(__dirname, '..', '__fixtures__', filename);
 
-test('genDiff test for flat json files', () => {
+test('genDiff test for flat JSON files', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const res = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+  expect(genDiff(filepath1, filepath2)).toEqual(res);
+});
+
+test('genDiff test for flat YML files', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const res = `{
